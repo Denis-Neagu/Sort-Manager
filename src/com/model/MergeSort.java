fff2bd4c3b3627046f3a5e549344e6499a216075
Denis-Neagu <denis.neagu@outlook.com>
2021-09-28 18:45:06 +0100

    implemented merge sort algorithm

--- src/com/model/MergeSort.java
+++ src/com/model/MergeSort.java
@@ -3,6 +3,31 @@ package com.model;
 public class MergeSort implements Sort {
     @Override
     public void sort(int[] array) {
+        sort(array, 0, array.length-1);
+    }
 
+    private void sort(int[] array, int start, int end) {
+        int mid = (start + end) / 2;
+        if (start < end) {
+            sort(array, start, mid);
+            sort(array, mid + 1, end);
+        }
+
+        int i = 0, first = start, last = mid + 1;
+        int[] tmp = new int[end - start + 1];
+
+        while (first <= mid && last <= end) {
+            tmp[i++] = array[first] < array[last] ? array[first++] : array[last++];
+        }
+        while (first <= mid) {
+            tmp[i++] = array[first++];
+        }
+        while (last <= end) {
+            tmp[i++] = array[last++];
+        }
+        i = 0;
+        while (start <= end) {
+            array[start++] = tmp[i++];
+        }
     }
 }
