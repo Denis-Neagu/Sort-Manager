548d9e51717ebcd1342c59dd6c227b028790cef0
Denis-Neagu <denis.neagu@outlook.com>
2021-09-28 18:36:45 +0100

    implemented bubble sort algorithm

--- src/com/model/BubbleSort.java
+++ src/com/model/BubbleSort.java
@@ -3,6 +3,18 @@ package com.model;
 public class BubbleSort implements Sort {
     @Override
     public void sort(int[] array) {
-
+        if(array == null || array.length == 0) {
+            System.out.println("array is null or empty");
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                for(int j = i + 1; j < array.length; j++) {
+                    if(array[i] > array[j]) {
+                        int temp = array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+        }
     }
 }
