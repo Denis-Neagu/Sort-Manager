2072e577be07c064dd2a36493f620def2c0127b4
Denis-Neagu <denis.neagu@outlook.com>
2021-10-02 20:06:16 +0100

    edited and added more tests to random array method

--- src/test/java/com/sparta/sortmanager/testing/RandomArrayTest.java
+++ src/test/java/com/sparta/sortmanager/testing/RandomArrayTest.java
@@ -2,7 +2,6 @@ package com.sparta.sortmanager.testing;
 
 import com.model.RandomArray;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RandomArrayTest {
@@ -11,6 +10,11 @@ public class RandomArrayTest {
     public void randomArray_ReturnsArray() {
         RandomArray randomArray = new RandomArray();
         int[] tempArr = randomArray.randomArray(10);
-        assertArrayEquals(tempArr,tempArr);
+
+        assertEquals(tempArr.length, 10);
+
+        for (int i = 0; i < tempArr.length; i++) {
+            assertNotNull(tempArr[i]);
+        }
     }
 }
